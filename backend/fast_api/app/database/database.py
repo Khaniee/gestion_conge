931d4ba53b1de4d0f4d5bc7ff67e687e4409ee40
Lovanirina R <lovanirina.ran@gmail.com>
2023-01-05 15:10:38 +0300

    fix when password is like "1234@1234"

--- backend/fast_api/app/database/database.py
+++ backend/fast_api/app/database/database.py
@@ -1,10 +1,11 @@
 from decouple import config
+from urllib import parse
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # SQL_ALCHEMY_URL = "bdd_type+driver://username:password@host:port/bdd_name"
-SQL_ALCHEMY_URL = f"postgresql://{config('DB_USERNAME')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+SQL_ALCHEMY_URL = f"postgresql://{config('DB_USERNAME')}:{parse.quote_plus(config('DB_PASSWORD'))}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
 
 engine = create_engine(SQL_ALCHEMY_URL)
 
