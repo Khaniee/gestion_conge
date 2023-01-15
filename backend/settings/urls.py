e4762774265fb5d5c12ed773a26390f275350099
Lovanirina R <lovanirina.ran@gmail.com>
2023-01-15 09:07:18 +0300

    Revert "fix backend structure"
    This reverts commit 0bced70059782eff05c4c7092a535d8de43f866d.

---
+++ backend/settings/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('gestion_conge.urls')),
+    path('api/', include('gestion_personne.urls'), name='api-gestion-personne')
+]
