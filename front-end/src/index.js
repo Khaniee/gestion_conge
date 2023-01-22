2a8f44aad35550f0d386bded8750de04e282f58c
Khanie <roseiloharaoke@gmail.com>
2023-01-22 18:16:24 +0300

    .

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -19,7 +19,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    {/* <Header /> */}
+    <Header />
     <Routes>
       <Route path="/" element={<App/>}/>
       <Route path="/login" element={<Login/>}/>
